1a5673b71aecfbf186a87dfe244709968af5a841
Nida Yucedal <yucedalnida@gmail.com>
2023-01-11 15:18:18 +0300

    new

--- src/test/java/stepdefinitions/HepsiStepDefs.java
+++ src/test/java/stepdefinitions/HepsiStepDefs.java
@@ -64,7 +64,6 @@ public class HepsiStepDefs {
         scrollAndClick((AndroidDriver<MobileElement>) Driver.getAppiumDriver(),"Mini Nutella Kakaolu Fındık Kreması 25g");
 
 
-
         //TouchActions action = new TouchActions(driver);
         //action.scroll(element, 10, 100);
         //action.perform();
