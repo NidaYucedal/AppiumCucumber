4f6323b0e9e8a004eee6515b3d2a9d29f6528bcf
Nida Yucedal <yucedalnida@gmail.com>
2023-03-04 11:17:56 +0300

    update

--- src/test/java/screens/HepsiScreen.java
+++ src/test/java/screens/HepsiScreen.java
@@ -53,7 +53,6 @@ public class HepsiScreen {
 
     @AndroidFindBy(xpath = "//android.widget.Button[@text='Alı\u015Fveri\u015Fi tamamla']")
     //android.widget.Button[@text='Alışverişi tamamla']
-
     public MobileElement completeShopping;
 
 
